bd3ad27875d5d321e63f6cdbebe12ffb2ceb4f79
Gerald McAlister <GEMISIS@users.noreply.github.com>
2025-08-18 20:36:23 -0700

    Force overwrite of known_hosts file

--- home/gemisis.nix
+++ home/gemisis.nix
@@ -22,6 +22,7 @@
   };
 
   home.file.".ssh/known_hosts" = {
+    force = true;
     text = ''
       github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
     '';
