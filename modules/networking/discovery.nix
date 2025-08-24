aa05d509ad36df27b983442ca4e2e5019aa1218f
Gerald McAlister <GEMISIS@users.noreply.github.com>
2025-08-24 12:38:46 -0700

    Add mDNS discovery module

---
+++ modules/networking/discovery.nix
@@ -0,0 +1,34 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.router.networking.discovery;
+  hw = config.router.hw;
+  vlans = config.router.vlans;
+  trunk = hw.trunk.iface;
+  mdnsPort = 5353;
+
+  allowedIfaces = map (v: "${trunk}.${toString v}") [ vlans.ha vlans.home vlans.media ];
+  firewallIfaces = genAttrs allowedIfaces (_: { allowedUDPPorts = [ mdnsPort ]; });
+in
+{
+  imports = [ ./vars.nix ];
+
+  options.router.networking.discovery.enable = mkOption {
+    type = types.bool;
+    default = false;
+    description = "Enable mDNS discovery bridging via Avahi reflector.";
+  };
+
+  config = mkIf cfg.enable {
+    services.avahi = {
+      enable = true;
+      reflector = true;
+      ipv6 = false;
+      allowInterfaces = allowedIfaces;
+    };
+
+    networking.firewall.interfaces = firewallIfaces;
+  };
+}
