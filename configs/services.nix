d3899ae97535e2744527bb9ad3e2f653eb39ce9a
Gerald McAlister <gemisis@users.noreply.github.com>
2025-11-15 13:14:14 -0800

    Enable Docker Support + Add Open WebUI Latest via Docker

--- configs/services.nix
+++ configs/services.nix
@@ -28,5 +28,35 @@
   };
 
   systemd.coredump.enable = true;
+
+  # Enable docker
+  virtualisation.docker.enable = true;
+
+  # Systemd unit that runs the Open WebUI container
+  systemd.services.open-webui-docker = {
+    description = "Open WebUI (Docker)";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" "docker.service" ];
+    wants = [ "network-online.target" "docker.service" ];
+
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "5s";
+
+      ExecStart = ''
+        ${pkgs.docker}/bin/docker run \
+          --name open-webui \
+          --rm \
+          -p 8008:8080 \
+          -v /var/lib/open-webui:/app/backend/data \
+          ghcr.io/open-webui/open-webui:0.6.36
+      '';
+
+      ExecStop = ''
+        ${pkgs.docker}/bin/docker stop open-webui || true
+      '';
+    };
+  };
 }
 
